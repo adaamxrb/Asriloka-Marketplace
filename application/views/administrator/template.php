2f75854466a883a99e308998faf391430dc9b945
adaamxrb <adamroybafiel@gmail.com>
2023-10-28 23:31:05 +0700

    clean

--- application/views/administrator/template.php
+++ application/views/administrator/template.php
@@ -1,10 +1,11 @@
-<?php 
-if ($this->session->level==''){
-    redirect(base_url());
-}else{
+<?php
+if ($this->session->level == '') {
+  redirect(base_url());
+} else {
 ?>
-<!DOCTYPE html>
-<html>
+  <!DOCTYPE html>
+  <html>
+
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -37,53 +38,81 @@ if ($this->session->level==''){
     <link rel="stylesheet" href="<?php echo base_url(); ?>asset/admin/plugins/datepicker/datepicker3.css">
     <!-- Daterange picker -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>asset/admin/plugins/daterangepicker/daterangepicker-bs3.css">
-    <style type="text/css"> .files{ position:absolute; z-index:2; top:0; left:0; filter: alpha(opacity=0);-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)"; opacity:0; background-color:transparent; color:transparent; } </style>
+    <style type="text/css">
+      .files {
+        position: absolute;
+        z-index: 2;
+        top: 0;
+        left: 0;
+        filter: alpha(opacity=0);
+        -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
+        opacity: 0;
+        background-color: transparent;
+        color: transparent;
+      }
+    </style>
     <script type="text/javascript" src="<?php echo base_url(); ?>/asset/admin/plugins/jQuery/jquery-1.12.3.min.js"></script>
     <script src="<?php echo base_url(''); ?>asset/ckeditor/ckeditor.js"></script>
-    <style type="text/css">#example thead tr, #table1 thead tr, #example1 thead tr, #example2 thead tr{ background-color: #e3e3e3; } .checkbox-scroll { border:1px solid #ccc; width:100%; height: 114px; padding-left:8px; overflow-y: scroll; }</style>
-      <link rel="stylesheet" href="https://almsaeedstudio.com/themes/AdminLTE/plugins/pace/pace.min.css">
+    <style type="text/css">
+      #example thead tr,
+      #table1 thead tr,
+      #example1 thead tr,
+      #example2 thead tr {
+        background-color: #e3e3e3;
+      }
+
+      .checkbox-scroll {
+        border: 1px solid #ccc;
+        width: 100%;
+        height: 114px;
+        padding-left: 8px;
+        overflow-y: scroll;
+      }
+    </style>
+    <link rel="stylesheet" href="https://almsaeedstudio.com/themes/AdminLTE/plugins/pace/pace.min.css">
     <script type="text/javascript">
-    function nospaces(t){
-        if(t.value.match(/\s/g)){
-            alert('Maaf, Tidak Boleh Menggunakan Spasi,..');
-            t.value=t.value.replace(/\s/g,'');
+      function nospaces(t) {
+        if (t.value.match(/\s/g)) {
+          alert('Maaf, Tidak Boleh Menggunakan Spasi,..');
+          t.value = t.value.replace(/\s/g, '');
         }
-    }
+      }
     </script>
   </head>
-  <body class="hold-transition skin-red-light sidebar-mini">
+
+  <body class="hold-transition skin-green-light sidebar-mini">
     <div class="wrapper">
       <header class="main-header">
-          <?php include "main-header.php"; ?>
+        <?php include "main-header.php"; ?>
       </header>
 
       <aside class="main-sidebar">
-          <?php include "menu-admin.php"; ?>
+        <?php include "menu-admin.php"; ?>
       </aside>
 
       <div class="content-wrapper">
 
-        <?php if ($this->uri->segment(2)=='home'){ ?>
-        <div class='alert alert-warning alert-dismissible fade in' role='alert' style='border-radius:0px; margin-bottom:0px'>
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-          <a href="https://members.phpmu.com/" style="margin-right:10px; text-decoration:none;">Haloo Selamat datang di halaman administrator, Saat ini anda menggunakan CMS Swarakalibata</a>
-          <a target='_BLANK' class="btn btn-default btn-sm" href="https://members.phpmu.com/" style="color: rgb(243, 156, 18);">Marketplace 1.0</a>
-        </div>
-      <?php } ?>
+        <?php if ($this->uri->segment(2) == 'home') { ?>
+          <div class='alert alert-warning alert-dismissible fade in' role='alert' style='border-radius:0px; margin-bottom:0px'>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <a href="https://members.phpmu.com/" style="margin-right:10px; text-decoration:none;">Haloo Selamat datang di halaman administrator</a>
+            <a target='_BLANK' class="btn btn-default btn-sm" href="https://github.com/adaamxrb/" style="color: rgb(243, 156, 18);">Asriloka Marketplace 1.0</a>
+          </div>
+        <?php } ?>
 
         <section class="content-header">
           <h1> Dashboard <small>Control panel </small> </h1>
         </section>
 
         <section class="content">
-            <?php echo $contents; ?>
+          <?php echo $contents; ?>
         </section>
         <div style='clear:both'></div>
       </div><!-- /.content-wrapper -->
       <footer class="main-footer">
-          <?php include "footer.php"; ?>
+        <?php include "footer.php"; ?>
       </footer>
     </div><!-- ./wrapper -->
     <!-- jQuery 2.1.4 -->
@@ -91,7 +120,9 @@ if ($this->session->level==''){
     <!-- jQuery UI 1.11.4 -->
     <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-    <script>$.widget.bridge('uibutton', $.ui.button);</script>
+    <script>
+      $.widget.bridge('uibutton', $.ui.button);
+    </script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/modules/data.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
@@ -124,9 +155,9 @@ if ($this->session->level==''){
     <script src="<?php echo base_url(); ?>asset/admin/dist/js/app.min.js"></script>
     <script src="<?php echo base_url(); ?>asset/admin/dist/js/jquery.nestable.js"></script>
     <script>
-    $('#rangepicker').daterangepicker();
-    $('.datepicker').datepicker();
-      $(function () { 
+      $('#rangepicker').daterangepicker();
+      $('.datepicker').datepicker();
+      $(function() {
         $("#example1").DataTable();
         $('#example2').DataTable({
           "paging": true,
@@ -138,34 +169,40 @@ if ($this->session->level==''){
         });
       });
     </script>
-  <script>
-    CKEDITOR.replace('editor1' ,{
-      filebrowserImageBrowseUrl : '<?php echo base_url('asset/kcfinder'); ?>'
-    });
-  </script>
-  <script type="text/javascript">
-  // To make Pace works on Ajax calls
-  $(document).ajaxStart(function() { Pace.restart(); });
-    $('.ajax').click(function(){
-        $.ajax({url: '#', success: function(result){
+    <script>
+      CKEDITOR.replace('editor1', {
+        filebrowserImageBrowseUrl: '<?php echo base_url('asset/kcfinder'); ?>'
+      });
+    </script>
+    <script type="text/javascript">
+      // To make Pace works on Ajax calls
+      $(document).ajaxStart(function() {
+        Pace.restart();
+      });
+      $('.ajax').click(function() {
+        $.ajax({
+          url: '#',
+          success: function(result) {
             $('.ajax-content').html('<hr>Ajax Request Completed !');
-        }});
-    });
+          }
+        });
+      });
 
 
-    /** add active class and stay opened when selected */
-    var url = window.location;
+      /** add active class and stay opened when selected */
+      var url = window.location;
 
-    // for sidebar menu entirely but not cover treeview
-    $('ul.sidebar-menu a').filter(function() {
-       return this.href == url;
-    }).parent().addClass('active');
+      // for sidebar menu entirely but not cover treeview
+      $('ul.sidebar-menu a').filter(function() {
+        return this.href == url;
+      }).parent().addClass('active');
 
-    // for treeview
-    $('ul.treeview-menu a').filter(function() {
-       return this.href == url;
-    }).parentsUntil(".sidebar-menu > .treeview-menu").addClass('active');
-  </script>
+      // for treeview
+      $('ul.treeview-menu a').filter(function() {
+        return this.href == url;
+      }).parentsUntil(".sidebar-menu > .treeview-menu").addClass('active');
+    </script>
   </body>
-</html>
+
+  </html>
 <?php } ?>
